abd5cddee5b212be0815118fd427095abebb2bcc
joshua-117 <msh0117@kookmin.ac.kr>
2019-09-14 01:28:41 +0900

    Edit regex

--- KCCM/app/src/main/java/com/kcc/kccm_project/service/logic/SignServiceLogic.java
+++ KCCM/app/src/main/java/com/kcc/kccm_project/service/logic/SignServiceLogic.java
@@ -4,7 +4,7 @@ import com.kcc.kccm_project.Entity.UserInfo;
 import com.kcc.kccm_project.service.SignService;
 import com.kcc.kccm_project.store.SignStore;
 import com.kcc.kccm_project.store.logic.StoreFactoryLogic;
-import com.kcc.kccm_project.util.ConversionUtil;
+// import com.kcc.kccm_project.util.ConversionUtil;
 import com.kcc.kccm_project.util.signUtill.InvalidValueException;
 
 import java.util.regex.Matcher;
@@ -23,17 +23,17 @@ public class SignServiceLogic implements SignService
     @Override
     public String registerUser(UserInfo userInfo)
     {
-        // TODO: userINfo에 있는 내용중 유효하지 않는 데이터가 있을 경우 예외 처리 발생 기능 구현
+        // TODO: userInfo에 있는 내용 중 유효하지 않은 데이터가 있을 경우 예외 처리 발생 기능 구현
         if ( isValidEmail(userInfo.getEmail()) && isValidPassword(userInfo.getPassword()) )
             return signStore.create(userInfo);
 
         else
-            throw new InvalidValueException("Invalid Email or password");
+            throw new InvalidValueException("Invalid Email or Password");
     }
 
 
     @Override
-    public UserInfo findUser(String schoolNumer)
+    public UserInfo findUser(String schoolNumber)
     {
         return null;
     }
@@ -53,8 +53,8 @@ public class SignServiceLogic implements SignService
     private boolean isValidEmail(String inputEmail)
     {
         // e-mail regular expression: check input value is whether e-mail-form or not
-        final Pattern VALID_EMIAL_ADDRESS_REGEX = Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$", Pattern.CASE_INSENSITIVE);
-        Matcher matcher = VALID_EMIAL_ADDRESS_REGEX.matcher(inputEmail);
+        final Pattern VALID_EMAIL_ADDRESS_REGEX = Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$", Pattern.CASE_INSENSITIVE);
+        Matcher matcher = VALID_EMAIL_ADDRESS_REGEX.matcher(inputEmail);
 
         // return true if it's e-mail-form, else return false
         return matcher.find();
@@ -63,7 +63,7 @@ public class SignServiceLogic implements SignService
     private boolean isValidPassword(String inputPW)
     {
         // password regular expression: check input value is 8 to 15 length, and only alpha-numeric-form
-        final Pattern VALID_PASSWORD_REGEX = Pattern.compile("^[a-zA-Z0-9]+$");
+        final Pattern VALID_PASSWORD_REGEX = Pattern.compile("((?=.*[a-z])(?=.*\\d)(?=.*[A-Z])(?=.*[@#$%!]).{8,20})");
         Matcher matcher = VALID_PASSWORD_REGEX.matcher(inputPW);
 
         // return true if it's correct-password-form, else return false
